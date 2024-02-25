72035e94a5ced15d8f7004dba1218d9849845363
Hilol <imanuilyusupov@gmail.com>
2024-02-26 00:09:09 +0300

    update

--- JS/main.js
+++ JS/main.js
@@ -13,3 +13,23 @@ document.querySelectorAll('a[data-scroll]').forEach(anchor => {
         });
     });
 });
+
+
+// Link btn
+
+function redirectToInstagram() {
+
+    window.location.href = 'https://www.instagram.com/inul.xl?igsh=MW90NzFkYXBwOWxweg%3D%3D&utm_source=qr';
+}
+function redirectToTelegram() {
+
+    window.location.href = 'https://t.me/Inullllllll';
+}
+function redirectToWhatsapp() {
+
+    window.location.href = 'https://wa.clck.bar/79034443749';
+}
+function redirectToGmail() {
+
+    window.location.href = 'https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=CllgCJfsczfwCNxGcFDpQnglCFmJmGjQBxhtKwlcZWPSWrqghzbBlnCLBsqGbZtFXvsWDxNdZGq';
+}
